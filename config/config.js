ac4b25e4a9c81ceb6088a030318b3425166a74fd
chenhong231 <chenhong231@outlook.com>
2022-09-07 08:52:29 +0800

    linui grid component

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 const config = {
   appkey: 'Qji6qqJKMjtVuKvA',
   apiBaseUrl: 'http://localhost:8081/swagger2/v1/'
+  //apiBaseUrl:'http://se.talelin.com/v1/'
 }
 
 export {
--- pages/home/home.js
+++ pages/home/home.js
@@ -2,6 +2,7 @@
 
 import { Theme } from "../../model/theme";
 import { Banner } from "../../model/banner";
+import { Category } from "../../model/category";
 
 Page({
 
@@ -11,6 +12,7 @@ Page({
   data: {
     themeA: null,
     bannerB: null,
+    grid: [],
   },
 
   /**
@@ -23,9 +25,11 @@ Page({
   async initAllData() {
     const themeA = await Theme.getHomeLocationA()
     const bannerB = await Banner.getHomeLocationB()
+    const grid = await Category.getGridCategory()
     this.setData({
       themeA: themeA[0],
-      bannerB
+      bannerB,
+      grid
     })
   },
 
