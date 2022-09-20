09d35b59efc7ab365e1f26f069f27362ee6a02b3
chenhong231 <chenhong231@outlook.com>
2022-09-20 23:25:41 +0800

    add sku code

---
+++ components/models/fence-group.js
@@ -0,0 +1,96 @@
+import { Matrix } from "./matrix"
+import { Fence } from "./fence"
+
+class FenceGroup {
+  spu
+  skuList = []
+  fences = []
+
+  constructor(spu) {
+    this.spu = spu
+    this.skuList = spu.sku_list
+  }
+
+  getDefaultSku() {
+    const defaultSkuId = this.spu.default_sku_id
+
+    if(!defaultSkuId) {
+      return
+    }
+
+    const newSku = this.skuList.find(s => s.id === defaultSkuId)
+    return newSku
+  }
+
+  getSku(skuCode) {
+    const fullSkuCode = this.spu.id + '$' + skuCode
+    
+    const sku = this.spu.sku_list.find(s=> s.code === fullSkuCode)
+    return sku?sku:null
+  }
+
+  setCellStatusById(cellId, status) {
+    this.eachCell((cell) => {
+      if(cell.id === cellId) {
+        cell.status = status
+      }
+    })
+  }
+
+  setCellStatusByXY(x, y, status) {
+    this.fences[x].cells[y].status = status
+  }
+
+  initFences1() {
+    const matrix = this._createMatrix(this.skuList)
+    const fences = []
+    let currentJ = -1
+    matrix.each((element, i, j) => {
+      if(currentJ !== j) {
+        //开启一个新列，需要创建一个新的Fence
+        currentJ = j
+        fences[currentJ] = this._createFence(element)
+      }
+      fences[currentJ].pushValueTitle(element.value)
+    })
+  }
+
+  initFences() {
+    const matrix = this._createMatrix(this.skuList)
+    const fences = []
+    const AT = matrix.transpose()
+    AT.forEach( r => {
+      const fence = new Fence(r)
+      fence.init()
+      fences.push(fence)
+    })
+    this.fences = fences
+  }
+
+  eachCell(cb) {
+    for(let i = 0; i < this.fences.length; i++) {
+      for(let j = 0; j < this.fences[i].cells.length; j++) {
+        const cell = this.fences[i].cells[j]
+        cb(cell, i , j)
+      }
+    }
+  }
+
+  _createFence(element) {
+    const fence = new Fence()
+    return fence
+  }
+
+  _createMatrix(skuList) {
+    const m = []
+    skuList.forEach(sku => {
+      m.push(sku.specs)
+    })
+
+    return new Matrix(m)
+  }
+}
+
+export {
+  FenceGroup
+}
--- utils/util.js
+++ utils/util.js
@@ -14,8 +14,27 @@ const promisic = function (func) {
   };
 };
 
+
+const combination = function (arr, size) {
+  var r = [];
+  function _(t, a, n) {
+      if (n === 0) {
+          r[r.length] = t;
+          return;
+      }
+      for (var i = 0, l = a.length - n; i <= l; i++) {
+          var b = t.slice();
+          b.push(a[i]);
+          _(b, a.slice(i + 1), n - 1);
+      }
+  }
+  _([], arr, size);
+  return r;
+};
+
 //相当于使用了代理模式
 
 export {
-  promisic
+  promisic,
+  combination
 }
